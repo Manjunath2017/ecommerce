2db05b0e16040b878f19067cc1fe18cbb812268e
Manjunath Jadhav <mja@durvah.com>
2022-08-24 20:47:13 +0530

    DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,23 @@
 const mongoose=require('mongoose')
-const dotenv=require('dotenv').config({path:__dirname+'/./../../.env'})
-require('../../.env')
+// const dotenv=require('dotenv').config({path:__dirname+'/./../../.env'})
+// require('../../.env')
+const config = require('config')
+const db = config.get('MONGO_URII')
 
 
 console.log('process.env.MONGO_URL',process.env.MONGO_URI)
 const connectDB=async()=>{
   try{
-    const conn=await mongoose.connect(`${process.env.MONGO_URI}`,{
+    const conn=await mongoose.connect(`${db}`)
+    
+    // ,{
+
+/*     // const conn=await mongoose.connect(`${process.env.MONGO_URI}`,{
       useUnifiedTopology:true,
       useNewUrlParser:true,
       useCreateIndex:true,
-    })
-    console.log(` Connected: ${conn.connection.host}`)
+     }) */
+    console.log(` Connected host: ${conn.connection.host}`)
   }catch(error){
     console.error(`Error: ${error.message}`)
     process.exit(1)
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const app=require('express')()
 const products=require('./data/products')
-const dotenv=require('dotenv').config({path:__dirname+'/./../.env'})
-// const connectDB=require('./config/db')
+// const dotenv=require('dotenv').config({path:__dirname+'/./../.env'})
+const connectDB=require('./config/db')
+const config = require('config')
 
-
-// connectDB()
+//connectDB
+connectDB()
 app.get('/', (req, res)=>{
   res.send('API is,, running...')
 })
@@ -19,7 +20,7 @@ app.get('/api/products/:id_no', (req,res)=>{
   res.json( product)
 })
 
-const port=process.env.PORT || 3000
+const port=config.get('PORT') || 3300
 console.log('port', port)
 app.listen(port, console.log(`server running in ${process.env.NODE_ENV} mode on port ${port}`))
 
